f29fd17d2aa7f0fcb2fbb1026f965b5512cd743c
AmMartian <123998872+AmmarSathar@users.noreply.github.com>
2024-09-22 01:07:02 -0400

    First commit
    basic section with landing page completed

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  const lenis = new Lenis({
+    smooth: true,
+    lerp: 0.1, // Adjusts the smoothness
+  });
+  
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  
+  requestAnimationFrame(raf);
+  
